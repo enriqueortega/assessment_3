42f5196ffb579425c38dd9df333304ba51664424
Enrique Ortega <enriqueortegatc@gmail.com>
2016-04-01 16:31:50 -0500

    corrected port for Scott

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var index = require('./modules/index.js');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 4000));
+app.set('port', (process.env.PORT || 4242));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
